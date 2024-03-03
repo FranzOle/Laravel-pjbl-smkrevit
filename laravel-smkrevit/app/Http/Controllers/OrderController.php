afe183d59051c170c39aecebd0a2f1f50965a92e
FranzOle <115760743+FranzOle@users.noreply.github.com>
2024-03-03 12:06:30 +0700

    Video 6

--- laravel-smkrevit/app/Http/Controllers/OrderController.php
+++ laravel-smkrevit/app/Http/Controllers/OrderController.php
@@ -14,6 +14,7 @@ class OrderController extends Controller
     public function index()
     {
         //
+        return view('Backend.order.select');
     }
 
     /**
--- laravel-smkrevit/routes/web.php
+++ laravel-smkrevit/routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Http\Controllers\CartControlle;
 use App\Http\Controllers\FrontController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CartController;
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\KategoriController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -39,3 +41,22 @@ Route::get('batal',[CartController::class,'batal']);
 Route::get('tambah/{idmenu}',[CartController::class,'tambah']);
 Route::get('kurang/{idmenu}',[CartController::class,'kurang']);
 Route::get('checkout',[CartController::class,'checkout']);
+
+Route::get('admin',[AuthController::class,'index']);
+Route::get('admin/logout',[AuthController::class,'logout']);
+Route::post('admin/postlogin',[AuthController::class,'postlogin']);
+Route::get('admin/logout',[AuthController::class,'logout']);
+
+Route::group(['prefix'=>'admin','middleware'=>['auth']],function(){
+    Route::group(['Middleware' => ['CekLogin:admin']],function(){
+        Route::resource('user',UserController::class);
+    });
+
+    Route::group(['Middleware' => ['CekLogin:kasir']],function(){
+        Route::resource('order',OrderController::class);
+    });
+
+    Route::group(['Middleware' => ['CekLogin:manager']],function(){
+        Route::resource('kategori',KategoriController::class);
+    });
+});
